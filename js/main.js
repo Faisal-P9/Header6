4798fff6e84a1100521081cb206e93123edf346d
Faisal-P9 <160835670+Faisal-P9@users.noreply.github.com>
2024-06-27 17:40:06 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(event){
+// document.addEventListener("DOMContentLoaded", function(event){
 
-    // document.addEventListener('readystatechange', function() {
+    document.addEventListener('readystatechange', function() {
+      document.body.style.visibility = 'visible';
 
-      if (document.readyState === 'interactive') {
+      if (document.readyState === 'complete') {
     // Hide the loading overlay and show the content
         document.getElementById('loader').style.display = 'none';
         document.getElementById('allContents').style.display = 'block';
